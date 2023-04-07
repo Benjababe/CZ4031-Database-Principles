d1d947788b6aa36f31ac2f9c1961df60294509fb
jun rong <jun.rong137@gmail.com>
2023-04-07 22:08:27 +0800

    Add qep output to interface

--- Project 2/interface.py
+++ Project 2/interface.py
@@ -19,6 +19,31 @@ import schemdraw
 from schemdraw.flow import *
 import cairosvg
 
+import psycopg2
+import psycopg2.extras
+from dotenv import load_dotenv
+import os
+
+from annotator import build_readable_tree, get_qep_difference, generate_numbered_list
+
+load_dotenv()
+
+connection = psycopg2.connect(
+    dbname=os.environ.get("DB_NAME"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    host=os.environ.get("DB_HOST"),
+    port=os.environ.get("DB_PORT"),
+)
+
+
+def get_query_execution_plan(connection, query: str):
+    with connection.cursor() as cursor:
+        cursor.execute(f"EXPLAIN (FORMAT JSON) {query}")
+        result = cursor.fetchone()
+        return result[0][0]["Plan"]
+
+
 sg.theme("SandyBeach")
 font = "Helvetica 14 bold"
 
@@ -38,41 +63,7 @@ leftColumn = [
     ],
 ]
 
-# rightColumn = [
-#     [
-#         sg.TabGroup(
-#             [
-#                 [
-#                     sg.Tab(
-#                         "Text description of QEP",
-#                         [
-#                             [
-#                                 sg.Text("QEP P1:", font=font),
-#                                 sg.Multiline(size=(50, 15), disabled=True, key="qepP1"),
-#                             ],
-#                             [
-#                                 sg.Text("QEP P2:", font=font),
-#                                 sg.Multiline(size=(50, 15), disabled=True, key="qepP2"),
-#                             ],
-#                         ],
-#                         key="textTab",
-#                     )
-#                 ],
-#                 [
-#                     sg.Tab(
-#                         "Graph view of QEP",
-#                         [[sg.Image(size=(500, 500), key="graphP1")]],
-#                         key="graphTab",
-#                     )
-#                 ],
-#             ],
-#             enable_events=True,
-#             key="tab",
-#         )
-#     ]
-# ]
-
-rightColumn2 = [
+centerColumn = [
     [
         sg.Text("QEP P1:", font=font),
         sg.Multiline(size=(50, 15), disabled=True, key="qepP1"),
@@ -81,6 +72,13 @@ rightColumn2 = [
         sg.Text("QEP P2:", font=font),
         sg.Multiline(size=(50, 15), disabled=True, key="qepP2"),
     ],
+]
+
+rightColumn = [
+    [
+        sg.Text("Difference:", font=font),
+        sg.Multiline(size=(50, 15), disabled=True, key="qepDiff"),
+    ],
     [sg.Button("View as graph", size=(20, 2), font=font, key="graph")],
 ]
 
@@ -88,7 +86,9 @@ layout = [
     [
         sg.Column(leftColumn),
         sg.VerticalSeparator(),
-        sg.Column(rightColumn2),
+        sg.Column(centerColumn),
+        sg.VerticalSeparator(),
+        sg.Column(rightColumn),
     ]
 ]
 
@@ -138,7 +138,7 @@ def preOrder(node, d, arrow, numChild, parentIndex, childIndex):
             leftDegree += 10
         else:
             d += Arrow().down()
-        nodeList[childIndex] = Start().label(node.data)
+        nodeList[childIndex] = Start(w=4).label(node.data)  # .fill("red")
         d += nodeList[childIndex]
         updatedParentIndex = childIndex
 
@@ -196,47 +196,64 @@ window = sg.Window("Project 2: QEP", layout)
 window2Active = False
 
 # Event loop to process events and get inputs
-while True:
-    event, values = window.read()
+def startGUI():
+    while True:
+        event, values = window.read()
 
-    # If the user closes the window or clicks the Exit button
-    if event == sg.WINDOW_CLOSED or event == "Exit":
-        break
+        # If the user closes the window or clicks the Exit button
+        if event == sg.WINDOW_CLOSED or event == "Exit":
+            break
 
-    # If the user clicks the Compare button
-    if event == "Compare":
-        # Get the input from the textboxes
-        query1 = values["query1"]
-        query2 = values["query2"]
+        # If the user clicks the Compare button
+        if event == "Compare":
+            # Get the input from the textboxes
+            query1 = values["query1"]
+            query2 = values["query2"]
 
-        # Check if the textboxes are empty or contains whitespaces
-        if not query1.strip() or not query2.strip():
-            sg.popup_error("Please enter queries in both boxes.", title="Error")
-        else:
-            try:
-                # Compare the queries and display the QEPs in the displayboxes
-                qepP1 = f'{values["query1"]}'
-                qepP2 = f'{values["query2"]}'
-                window["qepP1"].update(qepP1)
-                window["qepP2"].update(qepP2)
-            except Exception as e:
-                sg.popup_error(f"Error: {e}", title="Error")
+            # Check if the textboxes are empty or contains whitespaces
+            if not query1.strip() or not query2.strip():
+                sg.popup_error("Please enter queries in both boxes.", title="Error")
+            else:
+                try:
+                    # Compare the queries and display the QEPs in the displayboxes
+                    qep1 = get_query_execution_plan(connection, query1)
+                    qep2 = get_query_execution_plan(connection, query2)
+                    n1 = build_readable_tree(qep1)
+                    n2 = build_readable_tree(qep2)
+                    diff = get_qep_difference(n1, n2)
 
-    if event == "graph" and not window2Active:
-        window2Active = True
-        with schemdraw.Drawing(show=False) as d:
-            leftDegree = -170
-            rightDegree = -10
-            preOrder(input, d, None, 0, 0, 0)
-            d.save(fname="graph1.svg")
-            cairosvg.svg2png(url="graph1.svg", write_to="graph1.png")
-        layout2 = [[sg.Image(background_color="white", filename="graph1.png")]]
-        window2 = sg.Window("QEP1 Graph View", layout2, grab_anywhere=True)
+                    window["qepP1"].update(n1)
+                    window["qepP2"].update(n2)
+                    window["qepDiff"].update(generate_numbered_list(diff))
+                except Exception as e:
+                    sg.popup_error(f"Error: {e}", title="Error")
 
-    if window2Active:
-        event, values = window2.read(timeout=100)
-        if event == sg.WIN_CLOSED:
-            window2Active = False
-            window2.close()
-# Close the window
-window.close()
+        # if event == "graph" and not window2Active:
+        #     window2Active = True
+        #     with schemdraw.Drawing(show=False) as d:
+        #         leftDegree = -170
+        #         rightDegree = -10
+        #         preOrder(input, d, None, 0, 0, 0)
+        #         d.save(fname="graph1.svg")
+        #         cairosvg.svg2png(url="graph1.svg", write_to="graph1.png")
+        #     #     graph_image = PIL.Image.open("graph1.png")
+        #     #     if graph_image.size != (1000, 1000):
+        #     #         graph_image = graph_image.resize((1000, 1000), PIL.Image.ANTIALIAS)
+
+        #     # layout2 = [
+        #     #     [
+        #     #         sg.Image(
+        #     #             background_color="white", size=(1000, 1000), filename="graph1.png"
+        #     #         )
+        #     #     ]
+        #     # ]
+        #     layout2 = [[sg.Image(background_color="white", filename="graph1.png")]]
+        #     window2 = sg.Window("QEP1 Graph View", layout2, grab_anywhere=True)
+
+        # if window2Active:
+        #     event, values = window2.read(timeout=100)
+        #     if event == sg.WIN_CLOSED:
+        #         window2Active = False
+        #         window2.close()
+    # Close the window
+    window.close()
--- Project 2/project.py
+++ Project 2/project.py
@@ -5,6 +5,8 @@ import psycopg2.extras
 import annotator
 
 from dotenv import load_dotenv
+from interface import startGUI
+
 load_dotenv()
 
 connection = psycopg2.connect(
@@ -12,7 +14,7 @@ connection = psycopg2.connect(
     user=os.environ.get("DB_USER"),
     password=os.environ.get("DB_PASSWORD"),
     host=os.environ.get("DB_HOST"),
-    port=os.environ.get("DB_PORT")
+    port=os.environ.get("DB_PORT"),
 )
 
 
@@ -20,24 +22,24 @@ def get_query_execution_plan(connection, query: str):
     with connection.cursor() as cursor:
         cursor.execute(f"EXPLAIN (FORMAT JSON) {query}")
         result = cursor.fetchone()
-        return result[0][0]['Plan']
+        return result[0][0]["Plan"]
 
 
 def main():
-    query1 = '''
+    query1 = """
         select *
 from (SELECT supplier.s_nationkey,supplier.s_suppkey FROM supplier WHERE 200<s_suppkey) AS a
 join (SELECT nation.n_nationkey, nation.n_regionkey FROM nation) As b
 on a.s_nationkey = b.n_nationkey
-    '''
+    """
     qep1 = get_query_execution_plan(connection, query1)
 
-    query2 = '''
+    query2 = """
         select *
 from (SELECT supplier.s_nationkey,supplier.s_suppkey FROM supplier  WHERE 200>s_suppkey ORDER BY supplier.s_nationkey) AS a
 join (SELECT nation.n_nationkey, nation.n_regionkey FROM nation ORDER BY nation.n_nationkey) As b
 on a.s_nationkey = b.n_nationkey
-    '''
+    """
     qep2 = get_query_execution_plan(connection, query2)
 
     n1 = annotator.build_readable_tree(qep1)
@@ -56,4 +58,5 @@ on a.s_nationkey = b.n_nationkey
 
 
 if __name__ == "__main__":
-    main()
+    startGUI()
+    # main()
